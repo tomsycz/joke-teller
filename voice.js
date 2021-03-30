e6c869f1bf7a4c2fa275855c78c326a44abb6270
Thomas Syczyk <tomsycz@gmail.com>
2021-03-30 12:07:39 +0100

    voiceRSS exported from the script

--- voice.js
+++ voice.js
@@ -1,7 +1,5 @@
-const audioElement = document.getElementById("audio");
-const button = document.getElementById("button");
-
 // VoiceRSS Javascript SDK
+
 const VoiceRSS = {
   speech: function (e) {
     this._validate(e), this._request(e);
@@ -106,49 +104,3 @@ const VoiceRSS = {
     throw "The browser does not support HTTP request";
   },
 };
-
-//Disable/Enable Button
-function toggleButton() {
-  button.disabled = !button.disabled;
-  button.classList.toggle("inactive");
-}
-
-// Passing Joke to VoiceRSS API
-function tellMe(joke) {
-  VoiceRSS.speech({
-    key: config.API_KEY,
-    src: joke,
-    hl: "en-us",
-    v: "Alice",
-    r: 0,
-    c: "mp3",
-    f: "44khz_16bit_stereo",
-    ssml: false,
-  });
-}
-
-// Get Jokes froom Joke API
-
-async function getJoke() {
-  toggleButton();
-  let joke = "";
-  const apiUrl =
-    "https://v2.jokeapi.dev/joke/Dark?blacklistFlags=racist,sexist";
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    if (data.joke) {
-      joke = data.joke;
-    } else {
-      joke = data.setup + "\n" + data.delivery;
-    }
-    tellMe(joke);
-  } catch (error) {
-    // Catch Errors Here
-    console.log("whoops", error);
-  }
-}
-
-// Event Listeners
-button.addEventListener("click", getJoke);
-audioElement.addEventListener("ended", toggleButton);
